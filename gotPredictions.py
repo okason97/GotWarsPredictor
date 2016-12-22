7394abfc1ab3348a38031fd0a362c1306ca069dc
okason97 <okason1997@hotmail.com>
2016-12-22 05:40:20 -0300

    Update gotPredictions.py

--- gotPredictions.py
+++ gotPredictions.py
@@ -1,7 +1,7 @@
 from sklearn import tree
 from sklearn import preprocessing
-from numpy import ndarray
 
+#this is to give each house a number to easily classify them
 le = preprocessing.LabelEncoder()
 le.fit(['Lannister','Stark','noone', 'Tully', 'Baratheon', 'Darry', 'Greyjoy', 'Night\'s Watch',
        'Brave Companions', 'Mallister', 'Bolton', 'Karstark', 'Mormont', 'Thenns', 'Free folk', 'Blackwood', 
@@ -9,16 +9,15 @@ le.fit(['Lannister','Stark','noone', 'Tully', 'Baratheon', 'Darry', 'Greyjoy', '
 
 def extract_data(filename):
 	
-    #arreglos con las etiquetas y los vectores con atributos
+    #labels and features vectors
     labels = []
     fvecs = []
 
-    #salteo los nombres de los atributos
+    #first line has atribute names
     iterfile = iter(file(filename))
     next(iterfile)
   
-    #extraigo del archivo las etiquetas y vectores que necesito
-    #le.fit_transform hashea los nombres de las familias
+    #extract from file names for fvects and result of war for labels
     for line in iterfile:
         row = line.split(',')
         labels.append(str(row[13]))
@@ -26,16 +25,18 @@ def extract_data(filename):
 
     return labels, fvecs
 
-#extraigo la data de batallas de got
-#0 = resultado
+#extract battle data
+#0 = result
 #1 = [[atk1,atk2,atk3,atk4][def1,def2,def3,def4]]
 data = extract_data('./battles.csv')
 
 #clf = clasifier
 clf = tree.DecisionTreeClassifier()
 
+#fill decision tree
 clf = clf.fit(data[1],data[0])
 
+#predict using chosen data, in this case i'm predicting the outcome of a war between Lannister and Stark fammilies
 prediction_data = le.transform(['Lannister', 'noone', 'noone', 'noone', 'Mormont', 'Stark', 'noone', 'noone'])
 prediction = clf.predict(prediction_data)
 
